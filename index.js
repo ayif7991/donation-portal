95d69e0306194a9852625a790d9792ef1e1b7367
Alfiya <alfiya060@gmail.com>
2022-04-01 11:28:29 +0530

    static pages commit

--- index.js
+++ index.js
@@ -2,9 +2,50 @@ const express = require("express");
 const app = express();
 
 const port = 3000;
-app.use(express.static("public"));
+//To serve the Static pages
 app.use("/public", express.static("public"));
+//To serve the home  page
 app.get("/", function (req, res) {
   res.sendFile("./view/index.html", { root: __dirname });
 });
+
+//To serve the admin page
+app.get("/admin", function (req, res) {
+  res.sendFile("./view/admin.html", { root: __dirname });
+});
+//To serve ngo login page
+app.get("/ngo", function (req, res) {
+  res.sendFile("./view/ngologin.html", { root: __dirname });
+});
+//To serve ngo signup page
+app.get("/ngosignup", function (req, res) {
+  res.sendFile("./view/ngosignup.html", { root: __dirname });
+});
+//To serve donate page
+app.get("/Donate", function (req, res) {
+  res.sendFile("./view/Donate.html", { root: __dirname });
+});
+//To serve donatesignup page
+app.get("/Donatesignup", function (req, res) {
+  res.sendFile("./view/donatesignup.html", { root: __dirname });
+});
+//to serve donation type-education
+app.get("/Donationtype/education", function (req, res) {
+  res.sendFile("./view/donationtype/education.html", { root: __dirname });
+});
+//to serve donation type-food and water
+app.get("/Donationtype/foodandwater", function (req, res) {
+  res.sendFile("./view/donationtype/foodandwater.html", { root: __dirname });
+});
+//to serve donation type-health and medicine
+app.get("/Donationtype/healthandmedicine", function (req, res) {
+  res.sendFile("./view/donationtype/healthandmedicine.html", {
+    root: __dirname,
+  });
+});
+//To serve donatesignup page
+app.get("/Donationtype", function (req, res) {
+  res.sendFile("./view/donation_type.html", { root: __dirname });
+});
+
 app.listen(port, () => console.log(`listen to port${port}`));
